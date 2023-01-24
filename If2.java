cf3be98adb6ee6e4a2d910e20f8c61e9c9b406e7
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-01-24 17:22:42 +0530

    Add files via upload

---
+++ If2.java
@@ -0,0 +1,9 @@
+class If2
+{
+public static void main(String args[])
+{
+String name="sana";
+if(name=="sana")
+{
+System.out.println("name is equal to sana");
+}}}
---
+++ Ifelse3.java
@@ -0,0 +1,13 @@
+class Ifelse3
+{
+public static void main(String args[])
+{
+String name= "sana";
+if(name!= "sana")
+{
+System.out.println("you print correct name");
+}
+else
+{
+System.out.println("you print wrong name");
+}}}
