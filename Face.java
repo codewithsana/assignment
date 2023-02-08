64eb44beb342ecb96340bfc27c3ba69f5dd1d2ed
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-02-08 18:02:34 +0530

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,12 @@
+package objectandclass;
+
+public class Face {
+	public static void main(String[] args) {
+	    System.out.println(" +\"\"\"\"\"+");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | '-' |");
+		System.out.println(" +-----+");
+	}
+
+}
