3e22b03fc5960d592e9a1665aaf96aea1221d325
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-01 19:05:11 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,30 @@
+package mypackage;
+public class Movie {//class 1
+private String movieName;
+private String movieStartTime;
+private String movieEndTime;
+private int movieTicketPrice;
+public String getMovieName() {
+	return movieName;
+}
+public void setMovieName(String movieName) {
+	this.movieName = movieName;
+}
+public String getMovieStartTime() {
+	return movieStartTime;
+}
+public void setMovieStartTime(String movieStartTime) {
+	this.movieStartTime = movieStartTime;
+}
+public String getMovieEndTime() {
+	return movieEndTime;
+}
+public void setMovieEndTime(String movieEndTime) {
+	this.movieEndTime = movieEndTime;
+}
+public int getMovieTicketPrice() {
+	return movieTicketPrice;
+}
+public void setMovieTicketPrice(int movieTicketPrice) {
+	this.movieTicketPrice = movieTicketPrice;
+}}
