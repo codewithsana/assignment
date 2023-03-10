7aaa00e157316bc1a62620928568d3859d4726a2
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-10 14:58:47 +0530

    Add files via upload

---
+++ Array_Duplicate.java
@@ -0,0 +1,12 @@
+package Array;
+public class Array_Duplicate {//main class
+public static void main(String[] args) {//main method  
+//Initialize array  
+int arr[] = new int [] {1,2,3,4,1,7,8,8,3};  
+System.out.println("Duplicate elements in given array: ");  
+ //Searches for duplicate element  
+for(int i=0;i<arr.length;i++){  
+for(int j=i+1;j<arr.length;j++) {  
+ if(arr[i]==arr[j])  
+ System.out.println(arr[j]);  
+ }}}}
