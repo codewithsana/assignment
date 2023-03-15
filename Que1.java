e59e2294dbdcdae9abf36ac36e1a718da7c4dc1f
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-15 18:31:25 +0530

    Add files via upload

---
+++ Que1.java
@@ -0,0 +1,17 @@
+package String;
+public class Que1 {
+public static void main(String[] args) {
+String s="This a simple String";
+int n=5;
+int part=s.length()/n;
+while(n!=0) {
+	StringBuilder sb= new StringBuilder();
+	for(int i=0;i<part;i++) {
+	sb.append(s.charAt(i));	
+	}
+	System.out.println(sb);
+	s=s.substring(part);
+	n--;
+}
+}
+}
