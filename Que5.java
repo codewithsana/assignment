1181084c779c44bf9be472a84642f72b795ab7a9
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-03-15 18:32:32 +0530

    Add files via upload

---
+++ Que5.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Que5 {
+public static void main(String[] args) {
+	String str= "ABACD";
+	String str1="CDABA";
+	 String str2=str.concat(str1);//concatination
+	System.out.println(str2);
+	if(str.length()==str1.length()) {//rotation
+		System.out.println("yes! the string rotation exist");
+	}
+	else {
+		System.out.println("no! the string not rotation exist");
+	}
+	
+}
+}
