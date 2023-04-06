c9b51beac34804904190bb0e38ad597983cac161
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-04-06 21:49:16 +0530

    Add files via upload

---
+++ Exception_Assignment1.java
@@ -0,0 +1,63 @@
+package Exception;
+
+public class Exception_Assignment1 {
+	public static void main(String[] args)  {
+		String str="Sana";
+		//Division by zero
+		try {
+			  int a=100/0;
+				 System.out.println(a);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Invalid division");
+		}
+		//String parse to a numeric variable
+		try {
+				 String s1="Sana";
+				 System.out.println(Integer.parseInt(s1));
+		}
+		catch(Exception e)
+		{
+			System.out.println("Format mismatch");
+		}
+		//Accessing an invalid index
+		try {		 
+		String st="Sana";
+		char ch=st.charAt(10);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Index is Invalid");
+		}
+		//Accessing an invalid index in array
+		try {
+			 int arr[]=new int[5];
+   			  arr[7]=10;
+		}
+		catch(Exception e)
+		{
+			System.out.println("Array index is invaild");
+		}}
+		public void custom(int a)throws MyException{
+			
+		
+		try {
+			if(a>0)
+			throw new MyException("Sana");
+		}catch(MyException e) {
+			System.out.println("My Exception["+e.getMessage()+"]");
+		}
+		catch(Exception e) {
+			System.out.println("Exception encountered");
+		}
+		finally {
+			System.out.println("Exception handling completed");	
+			}}}
+class MyException extends Exception{
+	//private String Sana;
+	public MyException(String str) {
+//		this.Sana=Sana;
+		super(str);
+	}
+}
